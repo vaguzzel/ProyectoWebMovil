7d7a45d68445f0c4df7adfde6e19d952bfbf8422
Isidora Cisternas <127423300+IsidoraCis@users.noreply.github.com>
2025-04-19 18:52:20 -0400

    Se agregó funcionalidad de cambiar color de corazon al hacer clic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface WishlistItem {
+  id: number; // O string, un identificador único
+  brand: string;
+  name: string;
+  price: string;
+  imageUrl: string;
+  isLiked: boolean; // <-- La "bandera" para saber si está likeado
+}
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -12,11 +21,37 @@ export class Tab2Page {
   //    Inicialmente mostramos 'perfil'
   public currentView: string = 'perfil';
 
+  public wishlistItems: WishlistItem[] = [
+    {
+      id: 1,
+      brand: 'TOCOBO',
+      name: 'Cica Protector Solar En Barra Cotton Soft Sun Stick SPF50',
+      price: '$24.990',
+      imageUrl: 'assets/images/tocobo_protector.webp', // Asegúrate que la ruta es correcta
+      isLiked: true // Estado inicial: likeado (corazón rojo)
+    },
+    {
+      id: 2,
+      brand: 'RARE BEAUTY',
+      name: 'Bronceador en Barra Warm Wishes Effortless Bronzer Stick - Power Boost',
+      price: '$35.000',
+      imageUrl: 'assets/images/rare_beauty.webp', // Asegúrate que la ruta es correcta
+      isLiked: true // Estado inicial: likeado (corazón rojo)
+    },
+  ];
+
+
   constructor() {}
 
   // 2. Método para cambiar el estado actual
   changeView(viewName: string) {
     this.currentView = viewName;
     console.log('Vista cambiada a:', this.currentView); // Para depuración
+  }
+
+  // 3. Método para cambiar el estado de un elemento en la wishlist
+  toggleLike(item: WishlistItem) {
+    item.isLiked = !item.isLiked; // Cambia el estado de likeado a no likeado y viceversa
+    console.log('Estado actualizado para', item.name, ':', item.isLiked); // Para depuración
   }
 }
