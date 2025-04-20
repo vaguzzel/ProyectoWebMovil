654c18a6890f7c77507454ad9ba403191bcfbd0c
Valentina <valentinaguzman.el@gmail.com>
2025-04-19 22:01:15 -0400

    mejora en el inicio, pero aún está mal. COMPILA BIEN

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+// Añade esta interfaz en la parte superior del archivo, después de los imports
+interface SwiperContainer extends HTMLElement {
+  swiper?: any;
+}
+
+// Registrar elementos de Swiper para Angular
+register();
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false, // Mantener como no-standalone
+  standalone: false,
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
+  
+  // Referencias a los sliders para poder manipularlos
+  @ViewChild('ofertasSwiper') ofertasSwiper: ElementRef | undefined;
+  @ViewChild('masVendidosSwiper') masVendidosSwiper: ElementRef | undefined;
+  
   constructor() {}
+  
+  ngAfterViewInit() {
+    // Configurar los controles de navegación después de que la vista se haya inicializado
+    this.initializeSwiperNavigation();
+  }
+  
+  initializeSwiperNavigation() {
+    // Botones de navegación para Ofertas slider
+    const ofertasPrev = document.querySelector('.ofertas-prev');
+    const ofertasNext = document.querySelector('.ofertas-next');
+    
+    if (ofertasPrev) {
+      ofertasPrev.addEventListener('click', () => {
+        const swiperEl = document.querySelector('.ofertas-swiper') as SwiperContainer;
+        if (swiperEl && swiperEl.swiper) {
+          swiperEl.swiper.slidePrev();
+        }
+      });
+    }
+    
+    if (ofertasNext) {
+      ofertasNext.addEventListener('click', () => {
+        const swiperEl = document.querySelector('.ofertas-swiper') as SwiperContainer;
+        if (swiperEl && swiperEl.swiper) {
+          swiperEl.swiper.slideNext();
+        }
+      });
+    }
+    
+    // Botones de navegación para Más Vendidos slider
+    const mvPrev = document.querySelector('.mv-prev');
+    const mvNext = document.querySelector('.mv-next');
+    
+    if (mvPrev) {
+      mvPrev.addEventListener('click', () => {
+        const swiperEl = document.querySelector('.mas-vendidos-swiper') as SwiperContainer;
+        if (swiperEl && swiperEl.swiper) {
+          swiperEl.swiper.slidePrev();
+        }
+      });
+    }
+    
+    if (mvNext) {
+      mvNext.addEventListener('click', () => {
+        const swiperEl = document.querySelector('.mas-vendidos-swiper') as SwiperContainer;
+        if (swiperEl && swiperEl.swiper) {
+          swiperEl.swiper.slideNext();
+        }
+      });
+    }
+  }
 }
