cac03d1980cdbc3678cd8dc0889e0811d0acddf7
Rodolfo Fernández <rodolfo.fernandez.v@mail.pucv.cl>
2025-04-21 03:56:41 -0400

    Se añadió más navegación

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
@@ -8,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent  implements OnInit {
 
-  constructor() { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+  //funcion pa direccionar a mi perfil
+  goToTab4() {
+    this.router.navigate(['/tabs/tab4']);
+  }
+
+  goToTab5() {
+    this.router.navigate(['/tabs/tab5']);
+  }
+
 
 }
